5f8b59f1fc82c61c2b6fe99605269c09c9b84ee7
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-08-24 15:39:06 -0400

    working on home page

---
+++ src/components/home/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import Pic from "../../assets/images/pic.png";
+
+function Home() {
+  return (
+    <section className="home my-5">
+      <div className="title">
+        <h2>Hi, I'm Pete</h2>
+        <p>Welcome to my Portfolio!</p>
+        <Link to="portfolio">
+          <button>View my Projects</button>
+        </Link>
+      </div>
+      <div className="pic">
+        <img
+          src={Pic}
+          alt="profile pic"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Home;
