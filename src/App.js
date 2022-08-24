8182657398f319a618dba7a8c941b97b2da6a56c
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-08-24 15:16:30 -0400

    set up nav portion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { HashRouter, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Footer from './components/Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename="/">
+        <div className="app">
+          <Header />
+          <main>
+
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/portfolio" component={Portfolio} />
+
+          </main>
+          <Footer />
+        </div>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
