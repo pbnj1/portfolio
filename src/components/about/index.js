0421863754c0c283a3af63b8ee065df9ad4ec8d3
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-08-24 16:27:01 -0400

    updated about me

---
+++ src/components/about/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function About() {
+  return (
+    <section class="about">
+      <h1 id="about">About Me</h1>
+      <p>I am passionate about helping others improve quality of life through technology. 
+
+I am a software engineer drawing on my transferrable skills of 15 years as a physical therapist to create solutions to complex problems, root cause analysis, and cross functional communication. I am searching for a junior/associate software engineering position. 
+ 
+When I am not working I enjoy spending time with my family, hiking with our dog, and streaming too many hours of Netflix! I love animals, health and wellness, and doing my part to leave this world a better place.</p>
+    </section>
+  );
+}
+
+export default About;
